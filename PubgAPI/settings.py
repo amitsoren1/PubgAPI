632802e1a217e59ef3a9946560589b88915546aa
amitsoren1 <amt.sr.101@gmail.com>
2020-07-25 11:50:43 +0530

    updated host

--- PubgAPI/settings.py
+++ PubgAPI/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+8&*+197-p4+s+eg=dh#heyqj73gyq)z_zkq+@(qvsk-!o(ar@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
