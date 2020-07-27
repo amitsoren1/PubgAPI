6cea4a7b78f6dd88e83d56ccd1f8c916017fd3df
amitsoren1 <amt.sr.101@gmail.com>
2020-07-27 19:28:37 +0530

    added code to update players new and old

--- api/utils.py
+++ api/utils.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from api.models import Tournament,Match
+from api.models import Tournament,Match,Player
 
 HEADER = {
   "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJhZGVhMzQwMC1hZDhhLTAxMzgtM2IyMS0xYmJkOWE0ZjQyNzEiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTk1MzQwOTgzLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6Imx1Y2lmZXJoZWxsNjA2In0.NwsthIsl-D6uuiI0a7zwwk6gJrY1MeYwUudtCJYRXJc",
@@ -15,12 +15,13 @@ class UpdateData():
         tournaments = Tournament.objects.all()
         tournament_ids = []
         for tournament in tournaments:
-            tournament_ids.append(tournament.id)
+            tournament_ids.append(tournament.tournament_id)
         for tournament in response["data"]:
             if tournament["id"] not in tournament_ids:
                 obj = Tournament.objects.create(tournament_id=tournament["id"],
                                                 tournament=json.dumps(tournament))
                 obj.save()
+            print(tournament["id"], u'\u2713')
     
     def update_match(self,match_id,tournament_id):
         if Match.objects.filter(match_id=match_id).count()==0:
@@ -53,5 +54,44 @@ class UpdateData():
             response = json.loads(response.text)
             for resp in response["data"]["relationships"]["matches"]["data"]:
                 self.update_match(resp["id"],tournament.tournament_id)
-            print(u'\u2713')
-                
+            print(resp["id"], u'\u2713')
+
+    def update_players(self):
+        matches = Match.objects.all()
+        players = Player.objects.all()
+        player_ids = [player.player_id for player in players]
+        for match in matches:
+            match = json.loads(match.match)
+            for participant in match["included"]:
+                if participant["type"] == "participant" and participant["attributes"]["shardId"] == "steam":
+                    account_id = participant["attributes"]["stats"]["playerId"]
+                    if account_id not in player_ids:
+                        url = f"https://api.pubg.com/shards/steam/players/{account_id}"
+                        try:
+                            response = requests.get(url, headers=HEADER)
+                            response = json.loads(response.text)
+                        except:
+                            continue
+                        obj = Player.objects.create(player_id=account_id,player=json.dumps(response))
+                        obj.save()
+                        print(account_id, u'\u2713')
+                    else:
+                        try:
+                            existing_player = Player.objects.filter(player_id=account_id).first()
+                            existing_player_matches = []
+                            existing_player_details = json.loads(existing_player.player)
+                            for local_match in existing_player_details["data"][0]["relationships"]["matches"]["data"]:
+                                existing_player_matches.append(local_match.id)
+                            if match["data"]["id"] not in existing_player_matches:
+                                existing_player.delete()
+                                url = f"https://api.pubg.com/shards/steam/players/{account_id}"
+                                try:
+                                    response = requests.get(url, headers=HEADER)
+                                except:
+                                    continue
+                                obj = Player.objects.create(player_id=account_id,player=response)
+                                obj.save()
+                                print(account_id, u'\u2713')
+                        except:
+                            pass
+            break
