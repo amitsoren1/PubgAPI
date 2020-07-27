96cf8a76046037fd0761ac1945d0aaef88341d44
amitsoren1 <amt.sr.101@gmail.com>
2020-07-27 19:40:01 +0530

    updated models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Tournament(models.Model):
+    tournament_id = models.TextField(primary_key=True)
+    tournament = models.TextField(blank=True,null=True)
+
+    def __str__(self):
+        return f"{self.tournament_id}"
+
+class Match(models.Model):
+    match_id = models.TextField(primary_key=True)
+    tour_id = models.TextField(blank=True,null=True)
+    match = models.TextField(blank=True,null=True)
+
+    def __str__(self):
+        return f"{self.match_id}"
+
+class Player(models.Model):
+    player_id = models.TextField(primary_key=True)
+    player = models.TextField(blank=True,null=True)
+
+    def __str__(self):
+        return f"{self.player_id}"
