c296384eafb43166d62fb92e5874da48b43617e5
amitsoren1 <amt.sr.101@gmail.com>
2020-07-30 22:51:24 +0530

    registered player model

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Tournament,Match,Player
 # Register your models here.
+admin.site.register(Tournament)
+admin.site.register(Match)
+admin.site.register(Player)
