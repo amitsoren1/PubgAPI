00a17e7b76a1c11d967d624ebb968070b0388d8b
amitsoren1 <amt.sr.101@gmail.com>
2020-07-30 22:51:52 +0530

    updated urls of player

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,12 @@
-from api.views import ListUsers,ListMatches,ListTournaments,ListMatch,ListPlayers
+from api.views import List1User,List1Match,ListTournaments,ListMatches,ListPlayers
 from django.urls import path
 
 app_name = "api"
 
 # app_name will help us do a reverse look-up latter.
 urlpatterns = [
-    path('user/<str:user_id>', ListUsers.as_view()),
-    path('match/<str:match_id>',ListMatch.as_view()),
+    path('user/<str:user_id>', List1User.as_view()),
+    path('match/<str:match_id>',List1Match.as_view()),
     path('tournaments/',ListTournaments.as_view()),
     path('matches/',ListMatches.as_view()),
     path('players/',ListPlayers.as_view())
